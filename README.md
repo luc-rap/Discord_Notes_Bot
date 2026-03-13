04f143f6da9da937bafb19a00f8c124a52336dda
Lucia <lucia.rapanova@gmail.com>
2026-03-13 19:12:21 -0400

    updates to libraries, discord bot methods, join and leave voice channel

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Local and free personal Discord bot to provide notes from our DnD sessions on Di
 
 - **Whisper** is speech recognition system by OpenAI (open-sourced): fully local, runs on CPU/GPU, free, no OpenAI account needed, no data leaving computer (https://github.com/openai/whisper)
 
-- **discord\.py**
+- **discord\.py** to build the Discord bot (free, account needed)
 
-- **Ollama**
+- **Ollama** for local LLMs (free)
 
-- **Notion client** (optional)
+- **Notion client** (optional) - by default, the bot creates a Discord thread to post the meeting summary, but integration with Notion is also enabled
--- bot.py
+++ bot.py
@@ -1,20 +1,34 @@
 import discord
 from dotenv import load_dotenv
 import os
-
+from discord.ext.commands import Bot
+from discord.ext import commands
 load_dotenv()
 
-client = discord.Client(intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')   
+    print(f'We have logged in as {bot.user}')   
      
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')    
+#@bot.event
+#async def on_message(message):
+#    if message.author == bot.user:
+#        return
+#    if message.content.startswith('!hello'):
+#        await message.channel.send('Hello!')    
         
-client.run(os.getenv('DISCORD_TOKEN'))
+@bot.command()
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("You are not connected to a voice channel.")
+        
+@bot.command()
+async def leave(ctx):
+    await ctx.guild.voice_client.disconnect()
+        
+bot.run(os.getenv('DISCORD_TOKEN'))
+
